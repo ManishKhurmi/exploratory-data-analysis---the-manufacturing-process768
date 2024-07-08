ffd3f7813ec56e5a8fe0e4ff2516c716725996c4
ManishKhurmi <manish_khurmi@hotmail.co.uk>
2024-07-08 23:01:15 +0100

    created file for DataFrameInfo class

---
+++ DataFrameInfo.py
@@ -0,0 +1,12 @@
+# Describe all columns in the DataFrame to check their data types
+
+
+
+
+
+# Extract statistical values: median, standard deviation and mean from the columns and the DataFrame
+# Count distinct values in categorical columns
+# Print out the shape of the DataFrame
+# Generate a count/percentage count of NULL values in each column
+# Any other methods you may find useful
+
